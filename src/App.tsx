280559be2f4eb3f5831ba7b38c710e0b1ea9825d
Bastien Fontaine <b.fontaine@thecodingmachine.com>
2024-02-23 16:05:09 +0100

    this is recup baby

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
 import './App.css';
-import Card from "./components/Card.tsx";
 import CardGrid from "./components/CardGrid.tsx";
+import {useQueryClient, useQuery} from "@tanstack/react-query";
+import { request } from 'graphql-request'
+
+import { graphql } from './gql/gql';
+
+const TESTDATA = [
+    {"id": 1, "title": "foo"},
+    {"id": 2, "title": "bar"},
+]
+
+const charactersDocument = graphql(`
+    query charactersQuery($page: Int) {
+        characters(page: $page) {
+            results {
+                name
+            }
+        }
+    }
+`)
+
 function App() {
+
+    const queryClient = useQueryClient();
+    const testQuery = useQuery({
+        queryKey: ['test'],
+        queryFn: () => request('https://rickandmortyapi.com/graphql', charactersDocument, { page: 1 })
+    })
+
+    if (testQuery.isLoading) return <div>Loading...</div>
+
     return (
         <div className="container">
-            <CardGrid></CardGrid>
+            {testQuery.data.map((item) => (
+                <h1 key={item.id}>{item.title}</h1>
+            ))}
+            {/*<CardGrid></CardGrid>*/}
         </div>
     );
 }
 
+function wait(duration: int) {
+    return new Promise((resolve: fn) => {
+        setTimeout(() : void => {
+            resolve();
+        }, duration);
+    });
+}
 export default App;
